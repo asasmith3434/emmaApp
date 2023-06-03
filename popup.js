3538a334304f9405dc78a5716fbde3432a09427c
Asa Smith <asasmith@Asas-MacBook-Air.local>
2023-06-02 21:16:51 -0400

    react and node js app now

--- popup.js
+++
@@ -1,6 +0,0 @@
-window.addEventListener('DOMContentLoaded', () => {
-    const params = new URLSearchParams(window.location.search);
-    if (params.get('submitted')) {
-      alert('Form submitted successfully!');
-    }
-  });
